ff618015464a7eb94272ce30c764a8867132db9c
kom-1010 <kom-1010@naver.com>
2020-06-14 15:30:02 +0900

    lombok 라이브러리 사용

--- src/main/java/kr/ac/jejunu/post/Post.java
+++ src/main/java/kr/ac/jejunu/post/Post.java
@@ -1,49 +1,12 @@
 package kr.ac.jejunu.post;
 
+import lombok.Data;
+
+@Data
 public class Post {
     private Integer id;
     private String title;
     private String content;
-    private int user_id;
+    private Integer user_id;
     private String created_at;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
 }
