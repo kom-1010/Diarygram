25d7d45c33b3fe7efb6d157ff58519ed32c2d681
kom-1010 <kom-1010@naver.com>
2020-06-15 21:15:50 +0900

    getData(), Exception(), error() 추가

--- src/main/java/kr/ac/jejunu/post/UserController.java
+++ src/main/java/kr/ac/jejunu/post/UserController.java
@@ -2,17 +2,35 @@ package kr.ac.jejunu.post;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
-    
+    private final PostDao postDao;
+
     @RequestMapping("/user")
-    public User getUser(@RequestParam("id") Integer id) {
-        return userDao.get(id);
+    public ModelAndView getData(@RequestParam("id") Integer id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", userDao.get(id));
+        modelAndView.addObject("post", postDao.get(id));
+        return modelAndView;
+    }
+
+    @RequestMapping("/exception")
+    public void Exception() {
+        throw new RuntimeException("RuntimeException");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }
